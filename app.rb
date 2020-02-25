fbe43ffef5ec3809f38dc65e719334e99a4cb180
utkarshgupta410 <utkarshgupta410@gmail.com>
2020-02-25 03:14:07 +0000

    Iteration 2

--- app.rb
+++ app.rb
@@ -16,8 +16,38 @@ end
 
 get "/newspaper" do
   # do everything else
+  
   results = Geocoder.search(params["location"])
+  @location = params["location"]
   lat_lng = results.first.coordinates
-  lat = lat_lng[0]
-  lng = lat_lng[1]
+  @lat = lat_lng[0]
+  @lng = lat_lng[1]
+  
+  ForecastIO.api_key = "ce3af6469e281682efbcc88d15afa7ff" 
+  
+  @forecast = ForecastIO.forecast(@lat,@lng).to_hash
+
+  @current_temp = @forecast["currently"]["temperature"]
+  @current_sum = @forecast["currently"]["summary"]
+
+  @forecastarray = @forecast["daily"]["data"]
+  @dailytemparray = []
+  @dailysumarray = []
+
+  for weather_on_day in @forecastarray
+    @dailytemparray << weather_on_day["temperatureHigh"]
+    @dailysumarray << weather_on_day["summary"]
+  end
+  
+
+
+  puts @forecast
+  puts @current_temp
+  puts @current_sum
+  puts @dailytemparray
+  puts @dailysumarray
+
+
+  view "newspaper"
+
 end
